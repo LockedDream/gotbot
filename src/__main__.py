11e471fd81f9c20f3e800118cf2ceaa1cf0fdcf9
LockedDream <christofer0518@gmail.com>
2025-05-14 06:58:28 -0500

    added got mimic and got mentions

--- src/__main__.py
+++ src/__main__.py
@@ -36,6 +36,23 @@ async def on_message(message):
         '<:gotenough:1370384012069044336>',
         '<:gotwtf:1370383982251868160>',
         '<:got:1370383947795533976>']
+    
+    truegot = [
+        '<got666:1137419671570747554>',
+        '<got17:1296102708993982516>',
+        '<gotsip:979065494508286053>',
+        '<gotjudged:1001592848862421083>',
+        '<gotdavid:1005132112753594408>',
+        '<gotblast:1096080392999665745>',
+        '<got9:997269024595521557>',
+        '<got2:962520319795556402>',
+        '<gotlurk:974758117113987163>',
+        '<gotstare:958064790180360283>',
+        '<gotdamned:993552515025096714>',
+        '<gotenough:960565590085480458>',
+        '<gotwtf:922546697350053908>',
+        '<got:960624295359438878>'
+    ]
 
     reactdamn = random.choice(reactgot)
 
@@ -43,20 +60,35 @@ async def on_message(message):
         return
 
     if 'got' in message.content.lower():
+        # reacts with a random got emoji when any message contains any possible "got"
 
-        print('got detected\n'
-        f'tried {reactdamn}'
+        print(f'got detected: {message.content}\n'
+        f'reacting: {reactdamn}'
         )
 
         await message.add_reaction(f'{reactdamn}')
 
+    if client.user.mentioned_in(message):
+        # responds with a random got emoji when a message mentions the gotbot
+
+        print(f'got mentioned: {message.content}\n'
+              f'posting: {reactdamn}'
+              )
+        
+        await message.channel.send(f'{reactdamn}')
+
+    if message.content in truegot:
+        # mimics a got when any message is just a got emoji
+
+        print(f'got spotted: {message.content}\n'
+              f'copying: {message.content}'
+              )
+        
+        await message.channel.send(f'{message.content}')
 
 if __name__ == "__main__":
     # Load the .env vars
     load_dotenv()
-
-    # Add the cogs
-    # asyncio.run(add_all_cogs(bot))
 
     try:
         token = os.environ['TOKEN']
